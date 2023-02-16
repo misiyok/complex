a666a877b893373baf05f100cd9b7633c3832372
Isiyok <muhammed.isiyok@siemens.com>
2023-02-17 01:27:47 +0300

    changed App.js console log at worker index.js

--- worker/index.js
+++ worker/index.js
@@ -10,11 +10,12 @@ const redisClient = redis.createClient({
 const sub = redisClient.duplicate();
 
 function fib(index) {
-    if(index < 2) return 1;
+    if (index < 2) return 1;
     return fib(index - 1) + fib(index - 2);
 }
 
 sub.on('message', (channel, message) => {
+    console.log('[worker] received message: ', message);
     redisClient.hset('values', message, fib(parseInt(message)));
 });
 sub.subscribe('insert');
